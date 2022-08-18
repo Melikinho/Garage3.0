2529aeefd6ffeb62ff807c896f5c821cc175ad6c
Melikinho <Jimmymelik10@gmail.com>
2022-08-18 10:00:56 +0200

    Creating new Service for MemberShip

---
+++ Garage3.0.Web/Services/IMemberShipSelectListSerivce.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Garage3._0.Web.Services
+{
+    public interface IMemberShipSelectListService
+    {
+        Task<IEnumerable<SelectListItem>> GetVehicleTypesAsync();
+    }
+
+}
